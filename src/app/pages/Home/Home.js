501a338c0b14bf083fb5165035a581f1b101e757
tlugat <teo.lugat@hetic.net>
2021-07-18 12:41:06 +0200

    set style variables (font-sizes, colors, breakpoints) and mixins (flex, grid, gap)

--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import {useAuth} from "app/hooks/useAuth";
 
+import styles from './Home.module.scss';
+
 function Home() {
  
   const auth = useAuth();
   
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Bienvenue {auth.user.name}</h1>
       <button onClick={() => auth.logout()}>Se deconnecter</button>
     </div>
