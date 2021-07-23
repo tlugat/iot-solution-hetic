80f04e7b39e4437fef688208e34f67b13de1c897
tlugat <teo.lugat@hetic.net>
2021-07-23 11:56:39 +0200

    add indicator light on machine card and machine profile

--- src/app/commons/Loader/Loader.js
+++ src/app/commons/Loader/Loader.js
@@ -1,37 +1,11 @@
-import "./Loader.scss";
+import styles from "./Loader.module.scss";
 
 function Loader() {
   return (
-    <div className="main">
-			<div className="l">
-					<div className="l__face l__face--front">
-							<div className="l__control"></div>
-							<div className="l__control"></div>
-							<div className="l__buttons">
-									<div className="l__button"></div>
-									<div className="l__button"></div>
-									<div className="l__button"></div>
-							</div>
-							<div className="l__c1">
-									<div className="l__c2">
-											<div className="l__clothes">
-													<div className="l__clothes-i"></div>
-													<div className="l__clothes-i"></div>
-											</div>
-									</div>
-							</div>
-					</div>
-					<div className="l__face l__face--back"></div>
-					<div className="l__face l__face--right"></div>
-					<div className="l__face l__face--left"></div>
-					<div className="l__face l__face--top"></div>
-					<div className="l__face l__face--bottom"></div>
-			</div>
-			<div className="s">
-					<div className="l__face s__shadow"></div>
-			</div>
+		<div className={styles.container}>
+			<iframe title="loader" src="https://embed.lottiefiles.com/animation/64579"></iframe>	
 		</div>
-  )
+		)
 }
 
 export default Loader
